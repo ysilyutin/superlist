d8ab1759b4306b539362a959bf2bbb31d8efbbdc
Yegor Silyutin <pyxis@Yegors-MacBook-Pro.local>
2015-03-22 22:48:22 -0700

    First unit test and url mapping, dummy view

--- functional_tests.py
+++ functional_tests.py
@@ -5,7 +5,7 @@ class NewVisitorTest(unittest.TestCase):
 	
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
+		self.browser.implicitly_wait(4)
 
 	def tearDown(self):
 		self.browser.quit()
