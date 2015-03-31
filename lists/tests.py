6dec4f9259eca07b3339cd9f74f6c2199ef9049d
Yegor Silyutin <ysilyutin@gmail.com>
2015-03-30 20:36:28 -0700

    Triangulation

--- lists/tests.py
+++ lists/tests.py
@@ -11,8 +11,25 @@ class HomePageTest(TestCase):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
 
+
+
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
 		expected_html = render_to_string('home.html')
 		self.assertTrue(response.content.decode(), expected_html)
+
+
+
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
+		response = home_page(request)
+		self.assertIn('A new list item', response.content.decode())
+		expected_html = render_to_string(
+			'home.html',
+			{'new_item_text': 'A new list item'}
+		)
+		self.assertEqual(response.content.decode(), expected_html)
